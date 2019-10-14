1368e7d0e655ded9e214a32592c33c72b5326846
John Best <jkbest@gmail.com>
2019-10-14 14:02:43 -0700

    Use 99% CIs instead of 95%

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,20 +14,24 @@ cpg2_randvec = zeros(n)
 for i in eachindex(cpg2_randvec)
     cpg2_randvec[i] = rand(CPG2)
 end
-cpg2_ci = cdf(Normal(), 0.975) *
+cpg2_ci = cdf(Normal(), 0.995) *
     sqrt(failprob(CPG2) * succprob(CPG2) / n)
+cpg1_lpdf = logpdf(CPG1, cpg1_rand)
+cpg0_lpdf = logpdf(CPG2, 0.0)
 
 ## Note that simultaneously testing three 95% confidence intervals mean all
 ## tests are expected to pass ~85% of the time. If one of those three fail,
 ## re-run.
 @testset "TweedieDistributions.jl" begin
+    @test eltype(Tw1) == Float64
+    @test eltype(CPG2) == Float64
     @test cpg1_rand ≥ 0
     @test any(cpg2_randvec .== 0)
     @test succprob(CPG1) == 1 - exp(-CPG1.μ^(2 - CPG1.p) / ((2 - CPG1.p) * CPG1.ϕ))
     @test abs(failprob(CPG2) - mean(cpg2_randvec .== 0)) < cpg2_ci
     @test abs(mean(CPG2) - mean(cpg2_randvec)) < sqrt(var(CPG2) / n)
-    @test (n - 1) * var(cpg2_randvec) / quantile(Chisq(n - 1), 0.975) <
+    @test (n - 1) * var(cpg2_randvec) / quantile(Chisq(n - 1), 0.995) <
         var(CPG2) <
-        (n - 1) * var(cpg2_randvec) / quantile(Chisq(n - 1), 0.025)
-
+        (n - 1) * var(cpg2_randvec) / quantile(Chisq(n - 1), 0.005)
+    # @test lo
 end
